265e4766fd15b6111deab55cdba695a8f09b671a
Ivan <68787293+silversit@users.noreply.github.com>
2024-07-03 19:33:56 +0300

    Update settings.py

--- StudensManagementSystem/StudensManagementSystem/settings.py
+++ StudensManagementSystem/StudensManagementSystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*+!-m+4y5ulem96uzsg!co02p3i4)13*mim(mpj90a=w%+%4s_"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
